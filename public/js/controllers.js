98ef4cae0d78206b8cda5f4525ac2c3e0c6e68b7
Josh Newsom <newsommeister@gmail.com>
2016-09-14 19:20:25 -0600

    Can repeat notes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,6 +2,7 @@ app.controller('mainCtrl', mainCtrl);
 
 function mainCtrl($rootScope) {
     $rootScope.audioContext = new window.AudioContext();
+    var gain = $rootScope.audioContext.createGain();
 }
 mainCtrl.$inject = ['$rootScope'];
 
@@ -9,12 +10,12 @@ app.controller('keyCtrl', keyCtrl);
 
 function keyCtrl($scope, $rootScope, pitchService) {
     var ctx = $rootScope.audioContext;
-    var osc = pitchService.createPitch($scope.pitch);
     $scope.play = function(pitch) {
         // console.log('played' + pitch);
         // console.log(osc);
+        var osc = pitchService.createPitch($scope.pitch);
         osc.start();
-        osc.stop(ctx.currentTime + 1);
+        osc.stop(ctx.currentTime + .5);
     }
 }
 keyCtrl.$inject = ['$scope', '$rootScope', 'pitchService'];
