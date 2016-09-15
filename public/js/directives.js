18120193880f475a7c6d7ca9f3db587e73fa5bb8
Josh Newsom <newsommeister@gmail.com>
2016-09-15 13:32:09 -0600

    Key listener refactor

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,130 +1,21 @@
-app.directive('keyListener', function() {
+app.directive('keyListener', function(listenerService) {
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
             var keyboard = element.find('keyboard');
             element.on('keydown', function(e) {
-                switch (e.keyCode) {
-                    //white keys
-                    case 65:
-                        var key = keyboard.find('keyboard-key')[0];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 83:
-                        var key = keyboard.find('keyboard-key')[1];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 68:
-                        var key = keyboard.find('keyboard-key')[2];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 70:
-                        var key = keyboard.find('keyboard-key')[3];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 71:
-                        var key = keyboard.find('keyboard-key')[4];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 72:
-                        var key = keyboard.find('keyboard-key')[5];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 74:
-                        var key = keyboard.find('keyboard-key')[6];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 75:
-                        var key = keyboard.find('keyboard-key')[7];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-
-                        // black keys
-                    case 87:
-                        var key = keyboard.find('keyboard-key')[8];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 69:
-                        var key = keyboard.find('keyboard-key')[9];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 84:
-                        var key = keyboard.find('keyboard-key')[10];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 89:
-                        var key = keyboard.find('keyboard-key')[11];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    case 85:
-                        var key = keyboard.find('keyboard-key')[12];
-                        angular.element(key).triggerHandler('mousedown');
-                        break;
-                    default:
-                }
+                var keyNum = listenerService.getKeyNum(e);
+                var key = keyboard.find('keyboard-key')[keyNum];
+                angular.element(key).triggerHandler('mousedown');
             });
             element.on('keyup', function(e) {
-                switch (e.keyCode) {
-                    //white keys
-                    case 65:
-                        var key = keyboard.find('keyboard-key')[0];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 83:
-                        var key = keyboard.find('keyboard-key')[1];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 68:
-                        var key = keyboard.find('keyboard-key')[2];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 70:
-                        var key = keyboard.find('keyboard-key')[3];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 71:
-                        var key = keyboard.find('keyboard-key')[4];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 72:
-                        var key = keyboard.find('keyboard-key')[5];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 74:
-                        var key = keyboard.find('keyboard-key')[6];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 75:
-                        var key = keyboard.find('keyboard-key')[7];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                        // black keys
-                    case 87:
-                        var key = keyboard.find('keyboard-key')[8];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 69:
-                        var key = keyboard.find('keyboard-key')[9];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 84:
-                        var key = keyboard.find('keyboard-key')[10];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 89:
-                        var key = keyboard.find('keyboard-key')[11];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    case 85:
-                        var key = keyboard.find('keyboard-key')[12];
-                        angular.element(key).triggerHandler('mouseup');
-                        break;
-                    default:
-                }
+                var keyNum = listenerService.getKeyNum(e);
+                var key = keyboard.find('keyboard-key')[keyNum];
+                angular.element(key).triggerHandler('mouseup');
             });
         }
     };
-});
+}).$inject = ['listenerService'];
 
 app.directive('keyboard', function() {
     return {
--- public/js/services.js
+++ public/js/services.js
@@ -22,11 +22,63 @@ function pitchService($rootScope) {
     }
 
     this.play = function(gain) {
-      gain.gain.value = 1;
+        gain.gain.value = 1;
     }
 
-    this.stop = function (gain) {
-      gain.gain.value = 0;
+    this.stop = function(gain) {
+        gain.gain.value = 0;
     }
 }
 pitchService.$inject = ['$rootScope']
+
+
+
+app.service('listenerService', listenerService);
+
+function listenerService() {
+    this.getKeyNum = function(e) {
+        switch (e.keyCode) {
+            //white keys
+            case 65:
+                return 0;
+                break;
+            case 83:
+                return 1;
+                break;
+            case 68:
+                return 2;
+                break;
+            case 70:
+                return 3;
+                break;
+            case 71:
+                return 4;
+                break;
+            case 72:
+                return 5;
+                break;
+            case 74:
+                return 6;
+                break;
+            case 75:
+                return 7;
+                break;
+            // black keys
+            case 87:
+                return 8;
+                break;
+            case 69:
+                return 9;
+                break;
+            case 84:
+                return 10;
+                break;
+            case 89:
+                return 11;
+                break;
+            case 85:
+                return 12;
+                break;
+        }
+    }
+}
